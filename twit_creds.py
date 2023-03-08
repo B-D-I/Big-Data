c0f28c4d17207a81af841db99be17377cbc7ae5b
Nathan <salomb3@gmail.com>
2023-03-08 12:34:28 +0000

    blank twitter creds file included

---
+++ twit_creds.py
@@ -0,0 +1,13 @@
+import tweepy
+
+cons_key = ''
+cons_secrt = ''
+accs_tokn = ''
+accs_tokn_scrt = ''
+br_tkn = ''
+
+client = tweepy.Client(
+    bearer_token='',
+    consumer_key='',
+    consumer_secret=''
+)
